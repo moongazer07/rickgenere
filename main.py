bdaa44a57255838cde391f022eab7006f8e45d91
moongazer07 <myboymohammed@gmail.com>
2022-01-23 13:44:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+print("credit goes to sr0812 on stackoverflow for the menu code")
+print("rickginere cipher menu")
+option = input("input 1 for encoder and 2 for decoder: ")
+if option == "1":
+    import encoder
+elif option == "2":
+    import decoder
